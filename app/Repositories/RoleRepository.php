e02483262678c8e2a66f4d1931633da56bee613a
Tiana Coker <tianacoker1@gmail.com>
2022-05-23 16:17:15 +0100

    generate frontend files using Infyom Generator

---
+++ app/Repositories/RoleRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Role;
+use InfyOm\Generator\Common\BaseRepository;
+
+/**
+ * Class RoleRepository
+ * @package App\Repositories
+ * @version May 23, 2022, 3:05 pm UTC
+ *
+ * @method Role findWithoutFail($id, $columns = ['*'])
+ * @method Role find($id, $columns = ['*'])
+ * @method Role first($columns = ['*'])
+*/
+class RoleRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name'
+    ];
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Role::class;
+    }
+}
