9e602b921797d46f61c72280e08cc5f8fca8c768
Tiana Coker <tianacoker1@gmail.com>
2022-06-06 23:39:57 +0100

    adds payout button and authorisation on buttons

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="content-header">
-        <h1>
+        <h1 class="pull-left">
             Account: {{ $account->id }}
 
             <small>    
@@ -16,8 +16,34 @@
 
             </small>
         </h1>
+
+        <h1 class="pull-right">
+@if(Auth::user()->id == $account->user_id)
+            
+            {!! Form::open(['route' => ['roles.destroy', $account->id], 'method' => 'delete', 'class'=> 'pull-left']) !!}
+        
+            {!! Form::button('<i class="glyphicon glyphicon-ok"></i>Apply for Payout', ['type' => 'submit', 'class' => 'btn btn-primary', 'onclick' => "return confirm('Are you sure you want to apply for payout?')"]) !!}
+        
+            {!! Form::close() !!}
+@endif
+
+@if(Auth::user()->role_id < 3)
+            {!! Form::open(['route' => ['roles.destroy', $account->id], 'method' => 'delete', 'class'=> 'pull-right', 'style'=> 'margin-left:10px']) !!}
+        
+            {!! Form::button('<i class="glyphicon glyphicon-ok"></i> Mark as Paid', ['type' => 'submit', 'class' => 'btn btn-primary', 'onclick' => "return confirm('Are you sure?')"]) !!}
+        
+            {!! Form::close() !!}
+@endif
+
+        </h1>
+
+
+        
+        
+
     </section>
     <div class="content">
+        <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
