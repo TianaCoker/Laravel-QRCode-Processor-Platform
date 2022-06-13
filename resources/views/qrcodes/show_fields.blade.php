2cb00eba39ad8f54895ceb05805c8812c36cdb23
Tiana Coker <tianacoker1@gmail.com>
2022-06-13 04:28:52 +0100

    add external form for users not logged in

--- resources/views/qrcodes/show_fields.blade.php
+++ resources/views/qrcodes/show_fields.blade.php
@@ -95,39 +95,26 @@
                 </div>
 
 
+                @if(!Auth::guest())
 
+                @include('qrcodes.paystack-form')
 
-                <form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal" role="form">
-                    <div class="row" style="margin-bottom:40px;">
-                        <div class="col-md-8 col-md-offset-2">
-                            <p>
-                                <div>
-                                    Lagos Eyo Print Tee Shirt
-                                    ₦ 2,950
-                                </div>
-                            </p>
-                            <input type="hidden" name="email" value="iamnotolodo@gmail.com"> {{-- required --}}
-                            <input type="hidden" name="orderID" value="{{ $qrcode->id }}">
-                            <input type="hidden" name="amount" value="{{ $qrcode->amount}}"> {{-- required in kobo --}}
-                            <input type="hidden" name="quantity" value="1">
-                            <input type="hidden" name="currency" value="NGN">
-                            <input type="hidden" name="metadata" value="{{ json_encode($array = ['key_name' => 'value',]) }}" > {{-- For other necessary things you want to add to your payload. it is optional though --}}
-                            <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
-                            
-                            <input type="hidden" name="split_code" value="SPL_EgunGUnBeCareful"> {{-- to support transaction split. more details https://paystack.com/docs/payments/multi-split-payments/#using-transaction-splits-with-payments --}}
-                            <!-- <input type="hidden" name="split" value="{{-- json_encode($split) --}}"> {{-- to support dynamic transaction split. More details https://paystack.com/docs/payments/multi-split-payments/#dynamic-splits --}}
-                            {{-- csrf_field() -- }} {{-- works only when using laravel 5.1, 5.2 --}}-->
-                
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}"> {{-- employ this in place of csrf_field only in laravel 5.0 --}}
-                
-                            <p>
-                                <button class="btn btn-success btn-lg btn-block" type="submit" value="Pay Now!">
-                                    <i class="fa fa-plus-circle fa-lg"></i> Pay Now!
-                                </button>
-                            </p>
-                        </div>
+                @else
+
+                 <form action="" method="post"  role="form" class="col-md-6">
+                    <div class="form-group">
+                     <label for="email">Enter Your Email</label>
+                     <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" class="form-control">
                     </div>
-                </form>
+
+                    <p>
+                        <button class="btn btn-success btn-lg " type="submit" value="Pay Now!">
+                            <i class="fa fa-plus-circle fa-lg"></i> Pay Now!
+                        </button>
+                    </p>
+                 </form>
+
+                @endif
  </div>
 
  @if(!Auth::guest() && ($qrcode->user_id == Auth::user()->id || Auth::user()->role_id < 3))
